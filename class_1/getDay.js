b093612010e93d5f74600b9735e943f19259d610
CWKnightley <knightley@civicplus.com>
2017-06-15 15:59:48 -0500

    Homework
    Function that gives day of the week of the fourth of july.

--- class_1/getDay.js
+++ class_1/getDay.js
@@ -1,12 +1,3 @@
-
-function echo(arg) {
-    let printArea = document.getElementById("main");
-    let p = document.createElement("p");
-    p.textContent = arg;
-    printArea.appendChild(p);
-} 
-
-
 function getDay(date) {
   var dayOfWeek = new Date(date).getDay();    
   return isNaN(dayOfWeek) ? null : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dayOfWeek];
