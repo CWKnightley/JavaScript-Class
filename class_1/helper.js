b7bc862d021cae6ccf5f597ab1fd298dc81a6b04
CWKnightley <knightley@civicplus.com>
2017-06-15 16:07:16 -0500

    Created a new app

--- class_1/helper.js
+++ class_1/helper.js
@@ -5,11 +5,3 @@ function echo(arg) {
     p.textContent = arg;
     printArea.appendChild(p);
 } 
-
-
-function getDay(date) {
-  var dayOfWeek = new Date(date).getDay();    
-  return isNaN(dayOfWeek) ? null : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dayOfWeek];
-}
-
-getDay("1776-07-04");
